9d850c38d0faf117dca061e578413f15df9f8cca
IsmaeI Pallol <162565436+Chismaso@users.noreply.github.com>
2025-06-30 22:31:39 +0200

    primeros proyectos python
    lista con nombres los cuales imprimimos con un saludo con un bucle "for"

---
+++ proyect_1.py
@@ -0,0 +1,5 @@
+friends = ["Alex", "Ismael", "Sara", "Veronica"]
+for friend in friends:
+    for amigo in friends:
+        if friend != amigo:
+            print("hi " + friend + " and " + amigo)
---
+++ proyect_2.py
@@ -0,0 +1,6 @@
+# ESte codigo es igual que el del proyect_1 pero no se repiten los nombres
+friends = ["Alex", "Sara", "Ismael", "Veronica"]
+for i in range(len(friends)):
+    for j in range(i + 1, len(friends)): #así evitamos imprimir combinaciones repetidas o invertidas. 
+                                         #Por ejemplo: Si i = 0 (Alex), j empezará en 1 (Sara) y no volverá a 0
+        print("hi " + friends[i] + " and " + friends[j])
