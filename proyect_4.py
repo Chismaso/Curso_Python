5a3f6623c8d00dcd1cf70ada987556eb5ee0fd68
IsmaeI Pallol <162565436+Chismaso@users.noreply.github.com>
2025-06-30 23:16:36 +0200

    proyect_4
    cálculos matemáticos con python

---
+++ proyect_4.py
@@ -0,0 +1,15 @@
+# Calculos en python
+# Se añade - str() - para poder anidar el resultado(en numero) con la oración(String)
+# sin str() no deja y da error
+print("4 + 5 = " + str(4+5))
+#9
+print("9 x 7 = " + str(9*7))
+#63
+print("-1/4 = " + str(-1/4))
+#-0.25
+print("1/3 = " + str(1/3))
+#0.3333333333333333
+print("((2050/5)-32)/9 = " + str(((2050/5)-32)/9))
+#42.0
+print("2 elevado a 10 es = " + str(2**10)) # 2 elevado a 10 (** = "elevado a")
+#1024
