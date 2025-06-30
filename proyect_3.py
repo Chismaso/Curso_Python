cc6785b98b2a43fdebab4a51edb98dc700bbecdb
IsmaeI Pallol <162565436+Chismaso@users.noreply.github.com>
2025-06-30 22:54:39 +0200

    proyect_3
    escribir "Hello world!" 10 veces con y sin números delante

---
+++ proyect_3.py
@@ -0,0 +1,6 @@
+# si quisiera repetir hello world! 10 veces seria asi:
+for i in range(10):
+        print("Hello world!")
+# si lo queremos con un número por delante seria asi:
+for numero in range (1, 11):
+        print(str(numero) + " Hello world!")
