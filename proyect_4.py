7d1f2273c11b59c97742d82211d4d5c17ad5ae93
IsmaeI Pallol <162565436+Chismaso@users.noreply.github.com>
2025-07-01 00:55:08 +0200

    Better Comments
    se añadio la extension "Better Comments" para que sean mas legibles los comentarios (colores).

--- proyect_4.py
+++ proyect_4.py
@@ -1,6 +1,6 @@
 # Calculos en python
-# Se añade - str() - para poder anidar el resultado(en numero) con la oración(String)
-# sin str() no deja y da error
+#!Se añade - str() - para poder anidar el resultado(en numero) con la oración(String)
+#!sin str() no deja y da error
 print("4 + 5 = " + str(4+5))
 #9
 print("9 x 7 = " + str(9*7))
@@ -11,5 +11,5 @@ print("1/3 = " + str(1/3))
 #0.3333333333333333
 print("((2050/5)-32)/9 = " + str(((2050/5)-32)/9))
 #42.0
-print("2 elevado a 10 es = " + str(2**10)) # 2 elevado a 10 (** = "elevado a")
+print("2 elevado a 10 es = " + str(2**10)) #!2 elevado a 10 (** = "elevado a")
 #1024
