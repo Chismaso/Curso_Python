0eefbb36128b29809a55fa89877616a017666b96
IsmaeI Pallol <162565436+Chismaso@users.noreply.github.com>
2025-07-02 21:46:26 +0200

    Add lucky number script with and without function
    ✨ Introduce un script que calcula un “número de la suerte” basándose en la longitud de un nombre, usando tanto código directo como una función reutilizable.

---
+++ proyect_15.py
@@ -0,0 +1,20 @@
+#sin funcion
+name = "Kay"
+
+#?len es una funcion que devuelve la longitud de un objeto
+#!en este caso, devuelve la cantidad de caracteres del string name
+number = len(name) * 9
+
+print("Hello " + name + ". Your lucky number is " + str(number))
+
+name = "Cameron"
+number = len(name) * 9
+
+print("Hello " + name + ". Your lucky number is " + str(number))
+
+#con funcion
+def lucky_number(name):
+    number = len(name) * 9
+    return f"Hello {name}. Your lucky number is {number}"
+print(lucky_number("Kay"))
+print(lucky_number("Cameron"))
