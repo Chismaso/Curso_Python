0e21b85a48b2f19e3ad44435087768e6ec106485
IsmaeI Pallol <162565436+Chismaso@users.noreply.github.com>
2025-07-02 15:14:21 +0200

    proyect_8
    Este código evalúa si un número es positivo, cero o negativo usando estructuras condicionales if, elif y else.

---
+++ proyect_8.py
@@ -0,0 +1,10 @@
+#Este código evalúa si un número es positivo, cero o negativo 
+#usando estructuras condicionales #!if, elif y else.
+number = -4
+
+if number > 0:
+    print("el numero es positivo.")
+elif number == 0:
+    print("el numero es 0.")
+else:
+    print("el numero es negativo.")
