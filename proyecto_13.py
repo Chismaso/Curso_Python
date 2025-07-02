9e4106be9db9df683b134babfed5814c2544a2fa
IsmaeI Pallol <162565436+Chismaso@users.noreply.github.com>
2025-07-02 17:22:10 +0200

    Add division check to prevent division by zero
    Se añadió un script que verifica si el denominador es cero antes de realizar la división, mostrando un mensaje apropiado si se intenta dividir entre cero.

---
+++ proyecto_13.py
@@ -0,0 +1,11 @@
+# Este bloque verifica si el denominador es distinto de cero antes de realizar la división.
+# Si es distinto de cero, calcula el resultado y lo muestra.
+# Si es igual a cero, muestra un mensaje indicando que no se puede dividir entre 0.
+numerator = 10
+denominator = 0 
+if denominator != 0:
+    result = numerator / denominator
+    print("Result:", result)
+else:
+    print("El denominador es 0")
+    print("no se puede dividir entre 0")
